6953e57637f0dc1f9de05fdba4030d4af664f25e
ashfaque <rahamankmashfaque@gmail.com>
2025-11-12 21:51:59 +0600

    updating tree

--- tree.py
+++ tree.py
@@ -1,5 +1,36 @@
 # create a terminal node value
+from splitting_dataset import get_split
+
 
 def to_terminal(group):
     outcomes = [row[-1] for row in group]
     return max(set(outcomes), key = outcomes.count)
+
+
+def split(node, max_depth, min_size, depth):
+    left, right = node['groups']
+    del(node['groups'])
+
+    # check for a no split
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
+    
+    # check for max_depth
+    if depth >= max_depth:
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
+    
+    # process left child
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+    else:
+        node['left'] = get_split(left)
+        split(node['left'], max_depth, min_size, depth + 1)
+
+    # process right child
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+    else:
+        node['right'] = get_split(right)
+        split(node['right'], max_depth, min_size, depth + 1)
